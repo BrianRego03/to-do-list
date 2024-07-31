158e8c0f53c4ffc090c99e61e5ff84e6af408d4f
Brian rego <brian.rego03@gmail.com>
2024-07-31 18:19:41 +0530

    Write the function for clearscreen

--- src/clearWindow.js
+++ src/clearWindow.js
@@ -1,5 +1,10 @@
 const clearScreen=()=>{
 
+    let todoSec=document.querySelector(".todoSection");
+    while (todoSec.hasChildNodes()) {
+        todoSec.removeChild(todoSec.firstChild);
+      }
+
 }
 
 export {clearScreen};
