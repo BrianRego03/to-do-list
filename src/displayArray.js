418372cf78ee0b74508a7e72be0c2b1a72997716
Brian rego <brian.rego03@gmail.com>
2024-07-31 14:30:27 +0530

    Design and style the DisplayArray

--- src/displayArray.js
+++ src/displayArray.js
@@ -4,9 +4,38 @@ const displayArray=(arr)=>{
 
     for(let i=0;i<arr.length;i++){
         let todoSecChild=document.createElement('div');
-        todoSecChild.innerText=arr[i].title;
         todoSec.appendChild(todoSecChild);
         todoSecChild.classList.add("todoStyle");
+        
+        let checkButton=document.createElement("button");
+        todoSecChild.appendChild(checkButton);
+        checkButton.setAttribute("id",`todoButton${arr[i].index}`);
+        checkButton.classList.add("checkStyling");
+
+        let titleDiv=document.createElement("div");
+        titleDiv.classList.add("titleStyling");
+        titleDiv.innerText=arr[i].title;
+        todoSecChild.appendChild(titleDiv);
+        
+
+        let dateDiv=document.createElement("div");
+        dateDiv.classList.add("dateStyling");
+        dateDiv.innerText=arr[i].dueDate;
+        todoSecChild.appendChild(dateDiv);
+
+        let modifyDiv=document.createElement("div");
+        todoSecChild.appendChild(modifyDiv);
+        modifyDiv.classList.add("modifyContainer");
+
+        let editButton=document.createElement("button");
+        modifyDiv.appendChild(editButton);
+        editButton.setAttribute("id",`editButton${arr[i].index}`);
+        editButton.classList.add("editStyling");
+
+        let deleteButton=document.createElement("button");
+        modifyDiv.appendChild(deleteButton);
+        deleteButton.setAttribute("id",`deleteButton${arr[i].index}`);
+        deleteButton.classList.add("deleteStyling");
     }
 
 }
